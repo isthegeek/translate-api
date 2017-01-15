2d317f737cd05017c3978828e1aecc5f8826b9c6
Vijay Nandwani <born2bgeeky@gmail.com>
2017-01-15 12:17:25 +0530

    Finalized the api

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import json
 import subprocess
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from werkzeug import secure_filename
 from googleapiclient import discovery
 import httplib2
 from oauth2client.client import GoogleCredentials
+from google.cloud import translate
 
 DISCOVERY_URL = ('https://{api}.googleapis.com/$discovery/rest?'
                  'version={apiVersion}')
@@ -18,17 +19,7 @@ app = Flask(__name__)
 # This is the path to the upload directory
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
-app.config['ALLOWED_EXTENSIONS'] = set(['wav'])
-
-# For a given file, return whether it's an allowed type or not
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
-
-@app.route('/')
-def index():
-    return render_template('main_input_form.html')
-
+app.config['ALLOWED_MIMETYPE'] = set(['audio/wav'])
 
 @app.errorhandler(500)
 def server_error(e):
@@ -36,11 +27,13 @@ def server_error(e):
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
 
-@app.route('/submitted', methods=['POST'])
-def main_input_form_submit():
-    file = request.files['file']
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file_to_convert.wav']
+    file_language = request.form['language']
+    target = 'en'
     # Check if the file is one of the allowed types/extensions
-    if file and allowed_file(file.filename):
+    if file and file.content_type in app.config['ALLOWED_MIMETYPE']:
         # Make the filename safe, remove unsupported chars
         filename = secure_filename(file.filename)
         # Move the file form the temporal folder to
@@ -48,10 +41,10 @@ def main_input_form_submit():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         # Redirect the user to the uploaded_file route, which
         # will basicaly show on the browser the uploaded file
-        s = ['sox', '-v 0.98', 'uploads/'+filename, '--rate', '16k', '--bits','16', '--channels','1', 'audio.raw']
+        s = ['sox', '-v 0.98', 'uploads/'+filename, '--rate', '16k', '--bits','16', '--channels','1', 'audio_'+filename+'.raw']
         subprocess.call(s)
         #subprocess.call(['sox -v 0.98', "uploads/" + filename ,''], shell=True)
-        with open("audio.raw", 'rb') as speech:
+        with open('audio_'+filename+'.raw', 'rb') as speech:
             speech_content = base64.b64encode(speech.read())
         credentials = GoogleCredentials.get_application_default().create_scoped(
             ['https://www.googleapis.com/auth/cloud-platform'])
@@ -64,17 +57,18 @@ def main_input_form_submit():
                 'config': {
                     'encoding': 'LINEAR16',  # raw 16-bit signed LE samples
                     'sampleRate': 16000,  # 16 khz
-                    'languageCode': 'en-US',  # a BCP-47 language tag
+                    'languageCode': file_language,  # a BCP-47 language tag
                 },
                 'audio': {
                     'content': speech_content.decode('UTF-8')
                     }
                 })
         response = service_request.execute()
-        print response
-        return render_template(
-          'main_input_form_submit.html',
-          response=response)
+        text = response["results"][0]["alternatives"][0]["transcript"]
+        translate_client = translate.Client()
+        translation = translate_client.translate(text,target_language=target)
+        translated_text = translation['translatedText']
+        return jsonify(text=text, translated_text=translated_text)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
